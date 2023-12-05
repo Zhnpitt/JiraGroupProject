c058b4e98809f41edf4002563191e7b9e22611a3
likeZZhou <zhoulikekk@gmail.com>
2023-12-05 18:50:17 -0500

    Revert "test1"
    This reverts commit 89fbd4cee72844870b064d5e3085e95fb4b0a475.

--- src/test/java/base/BaseSetUp.java
+++ src/test/java/base/BaseSetUp.java
@@ -22,7 +22,7 @@ public class BaseSetUp {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
-    //22222211
+    //
     @AfterClass
     public void tearDown(){
         driver.close();
