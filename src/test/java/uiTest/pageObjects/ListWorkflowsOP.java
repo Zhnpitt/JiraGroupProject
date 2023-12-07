13712ab62f9601d2d786bb5871d26f2db9460fd7
tommyshang <56244127+tommyshang@users.noreply.github.com>
2023-12-06 21:13:43 -0800

    Create ListWorkflowsOP.java

---
+++ src/test/java/uiTest/pageObjects/ListWorkflowsOP.java
@@ -0,0 +1,41 @@
+package uiTest.pageObjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+//http://localhost:8080/secure/admin/workflows/ListWorkflows.jspa
+public class ListWorkflowsOP {
+
+
+
+    @FindBy(xpath = "//a[@id = \"add-workflow\"]")
+    public WebElement addWorkflowBtn;
+
+    @FindBy(xpath = "//input[@id = \"add-workflow-newWorkflowName\"]")
+    public WebElement workflowName;
+    @FindBy(xpath = "//input[@id = \"add-workflow-description\"]")
+    public WebElement description;
+
+    @FindBy(xpath = "//button[@id = \"add-workflow-submit\"]")
+    public WebElement workflowFormAddBtn;
+
+
+
+
+
+    public void clickAddWorkflowBtn(){addWorkflowBtn.click();}
+
+    public void enterWorkflowName(String s){
+        workflowName.sendKeys(s);
+    }
+
+    public void  enterDescription(String s){
+        description.sendKeys(s);
+    }
+
+    public void clickWorkflowFormAddBtn(){
+        workflowFormAddBtn.click();
+    }
+
+
+}
