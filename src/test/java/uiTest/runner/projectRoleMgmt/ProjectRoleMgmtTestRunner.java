bda0efbd8ce2e2f031c373c417fa674c623b5eaf
likeZZhou <zhoulikekk@gmail.com>
2023-12-11 15:02:58 -0500

    RUNNER

--- src/test/java/uiTest/runner/projectRoleMgmt/ProjectRoleMgmtTestRunner.java
+++ src/test/java/uiTest/runner/projectRoleMgmt/ProjectRoleMgmtTestRunner.java
@@ -1,6 +1,11 @@
 package uiTest.runner.projectRoleMgmt;
 
+import io.cucumber.testng.CucumberOptions;
 import uiTest.runner.UiTestRunner;
 
+@CucumberOptions(
+        features = {"src/test/resources/feature/ui/projectRoleMgmt.feature"},
+        glue = {"uiTest.stepDef.projectRoleMgmt"}
+)
 public class ProjectRoleMgmtTestRunner extends UiTestRunner {
 }
